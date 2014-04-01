3de33264b29f220e08a39a627a285fe31e1f1a07
bspkrs <bspkrs@gmail.com>
2014-04-01 11:35:38 -0400

    updated copyright year

--- src/bspkrs/mmv/gui/MappingGui.java
+++ src/bspkrs/mmv/gui/MappingGui.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2013 bspkrs
- * Portions Copyright (C) 2013 Alex "immibis" Campbell
+ * Copyright (C) 2014 bspkrs
+ * Portions Copyright (C) 2014 Alex "immibis" Campbell
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
  * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
--- src/immibis/bon/gui/Reference.java
+++ src/immibis/bon/gui/Reference.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 Alex "immibis" Campbell
+ * Copyright (C) 2014 Alex "immibis" Campbell
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
  * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
