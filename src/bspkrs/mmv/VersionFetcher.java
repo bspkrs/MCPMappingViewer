8332c2b475a198f7d8f4a003616f9e185650ebfa
bspkrs <bspkrs@gmail.com>
2015-12-08 01:56:44 -0500

    fix ordering of the versions list

--- src/bspkrs/mmv/VersionFetcher.java
+++ src/bspkrs/mmv/VersionFetcher.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2015 bspkrs
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ * THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package bspkrs.mmv;
 
 import java.io.BufferedReader;
@@ -9,8 +24,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-
-import bspkrs.mmv.version.NaturalOrderComparator;
 
 import com.google.gson.Gson;
 
@@ -36,7 +49,7 @@ public class VersionFetcher
                 for (String channel : ((Map<String, ArrayList<Double>[]>) json.get(mcVer)).keySet())
                     for (Double ver : ((Map<String, ArrayList<Double>>) json.get(mcVer)).get(channel))
                         versions.add(mcVer + "_" + channel + "_" + String.format("%.0f", ver));
-            Collections.sort(versions, Collections.reverseOrder(new NaturalOrderComparator()));
+            Collections.sort(versions, Collections.reverseOrder(new SplittedNaturalComparator("_")));
             return versions;
         }
         else
